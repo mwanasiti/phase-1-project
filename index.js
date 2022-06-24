14845195416fdb908f59cc06dae1c9badb63b79d
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-24 19:16:22 +0300

    mcj

--- index.js
+++ index.js
@@ -7,33 +7,41 @@
 	getData(2)
 	document.addEventListener('dblclick',()=>{
 		alert("WELCOME")
+		const form=document.querySelector('form')
+		form.addEventListener('submit', event=>{
+			event.preventDefault()
+			const restaurantId=form.input.value;
+			getData(restaurantId)
+		})
 	})
 })
-
 function getData(id){
 	fetch(`http://localhost:3000/restaurants/${id}`)
 	.then(res=>res.json())
 	.then(data=>restaurantInfo(data))
 }
 
+
+
 function restaurantInfo(data){
 	// console.log(data.name)
 	document.getElementById('name').innerHTML=data.name
 	document.getElementById('description').innerHTML=data.description
 	document.querySelector('img').src=data.image
-	const btn=document.querySelector('button')
-	let count=parseInt(document.querySelector('span').innerHTML)+=
-
-	btn.addEventListener('click',()=>{
-		// console.log(count ++);
-	})
+	// const btn=document.querySelector('button')
+	// let count=parseInt(document.querySelector('span').innerHTML)
+	addComment()
 }
+	// btn.addEventListener('click',()=>{
+	// 	// console.log(count ++);
+	// })
+
 
 function addComment(){
 	const form=document.getElementById('formComment')
-	form.addEventListener('submit',()=>{
+	form.addEventListener('submit',e=>{
 		e.preventDefault()
-		const post=form.comment.value;
+		const post=(form.comment.value);
 		postComment(post)
 	})
 }
