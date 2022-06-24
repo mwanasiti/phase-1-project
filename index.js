9f7cf8a50298cf3e75272721ac3a57af72f415f3
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-24 10:25:04 +0300

    ject

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-fetch('https://worldwide-restaurants.p.rapidapi.com/search', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch('https://worldwide-restaurants.p.rapidapi.com/search', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
 	document.addEventListener('DOMContentLoaded',()=>{
 	getData(3)
 })
 
 function getData(id){
-	fetch(`http://localhost:3000/restaurants`/${id}).then(res=>res.json()).then(data=>console.log(data))
+	fetch(`http://localhost:3000/restaurants/${id}`).then(res=>res.json()).then(data=>console.log(data))
 }
+
